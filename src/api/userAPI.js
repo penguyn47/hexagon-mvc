08ac0573b0917c565c6940207be00932690faf88
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-21 21:55:29 +0700

    fix-picture-size

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
-router.post('/', userController.createUser);
+router.post("/", userController.createUser);
 
 module.exports = router;
