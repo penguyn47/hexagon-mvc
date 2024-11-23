702210e4b2dca356d06c9f75b8679d57346ada24
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-23 10:14:52 +0700

    Change session

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ app.use(
   session({
     secret: "secret-key",
     resave: true,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 30,
     },
