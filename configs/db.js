2128336ba9ed5a3c9bebc3acacd6816ff0749843
MrPen <lan474444@gmail.com>
2024-11-27 13:09:53 +0700

    Branch development

--- configs/db.js
+++ configs/db.js
@@ -18,12 +18,12 @@ const sequelize = new Sequelize(
             acquire: 30000,                 // Thời gian tối đa (ms) để kết nối
             idle: 10000                     // Thời gian tối đa (ms) kết nối không sử dụng sẽ bị đóng
         },
-        dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false // Đặt `false` nếu chứng chỉ không được xác thực
-            }
-          }
+        // dialectOptions: {
+        //     ssl: {
+        //       require: true,
+        //       rejectUnauthorized: false // Đặt `false` nếu chứng chỉ không được xác thực
+        //     }
+        //   }
       
     }
 );
